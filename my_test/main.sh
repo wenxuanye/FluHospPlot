0a4a25dbb9a6f59ad0fa4d4feb7f4ca8d981c38b
wenxuanye <wey420@lehigh.edu>
2022-02-07 23:15:08 -0500

    test git actions

--- my_test/main.sh
+++ my_test/main.sh
@@ -2,11 +2,12 @@
 
 set -eux
 
-# 修改为你的 CSDN_ID
-CSDN_ID="sculpta"
 
 python3 preprocess.py
 python3 build.py
+
+git config --global user.email "wey420@lehigh.edu"
+git config --global user.name "wenxuanye"
 
 git add .
 git commit -m "update"
