0331b622f1f1b20690f38a72f910015ebd678872
wenxuanye <wey420@lehigh.edu>
2022-02-09 20:13:22 -0500

    main branch add main.yml

--- my_test/main.sh
+++ my_test/main.sh
@@ -9,8 +9,8 @@
 # python3 preprocess.py
 # python3 build.py
 
-# git config --global user.email "wey420@lehigh.edu"
-# git config --global user.name "wenxuanye"
+git config --global user.email "wey420@lehigh.edu"
+git config --global user.name "wenxuanye"
 
 # git add .
 # git commit -m "$TODAY Git actions auto run"
