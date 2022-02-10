482809578e9f3ad96dc059eb2644a1319a861d68
wenxuanye <wey420@lehigh.edu>
2022-02-10 12:46:31 -0500

    dev branch modify main.sh

--- my_test/main.sh
+++ my_test/main.sh
@@ -10,6 +10,7 @@ python3 build.py
 git config --global user.email "wey420@lehigh.edu"
 git config --global user.name "wenxuanye"
 
+git pull
 git add .
 git commit -m "$TODAY Git actions auto run"
 git push
