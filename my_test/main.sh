c8842eb12167f177ab3c5891a4c46a2c40a265e1
wenxuanye <wey420@lehigh.edu>
2022-03-15 12:06:46 -0400

    modify

--- my_test/main.sh
+++ my_test/main.sh
@@ -7,8 +7,8 @@
 # python3 preprocess.py
 # python3 build.py
 
-# git config --global user.email "wey420@lehigh.edu"
-# git config --global user.name "wenxuanye"
+git config --global user.email "wey420@lehigh.edu"
+git config --global user.name "wenxuanye"
 
 # git pull
 # git add .
