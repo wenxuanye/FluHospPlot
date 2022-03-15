2e6fec829a8404d9f1f2a5039ae78f1609924736
wenxuanye <wey420@lehigh.edu>
2022-03-15 12:03:37 -0400

    modify

--- my_test/main.sh
+++ my_test/main.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-set -eux
+# set -eux
 
-# today
-TODAY=$(date +%Y%m%d)
-python3 preprocess.py
-python3 build.py
+# # today
+# TODAY=$(date +%Y%m%d)
+# python3 preprocess.py
+# python3 build.py
 
-git config --global user.email "wey420@lehigh.edu"
-git config --global user.name "wenxuanye"
+# git config --global user.email "wey420@lehigh.edu"
+# git config --global user.name "wenxuanye"
 
-git pull
-git add .
-git commit -m "$TODAY Git actions auto run"
-git checkout main
-git merge dev
+# git pull
+# git add .
+# git commit -m "$TODAY Git actions auto run"
+# git checkout main
+# git merge dev
 
-git push -u origin main
+# git push -u origin main
 # push the update to main branch
 # git push
+make
